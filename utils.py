5863032429e1951400fd39714629d2d5b12da56f
JNPR\mnikoloski <violeta.trajkova@gmail.com>
2021-07-22 02:35:25 +0200

    utils so resize

--- utils.py
+++ utils.py
@@ -9,12 +9,21 @@ def crop(img, crop_size=60):
 
     return cropped
 
+image_width = 200
+image_height = 66
+
+def resize_image(image):
+    """
+    Resize the image to the input shape used by the network model
+    """
+    return cv2.resize(image, (image_width, image_height), cv2.INTER_AREA)
 
 def preprocess(image):
     """
     Combine all preprocess functions into one
     """
     image = crop(image)
+    image = resize_image(image)
     image = image.astype(np.float32)
     image = image/255.0
 
